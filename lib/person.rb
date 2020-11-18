8256cbd24dadc05edd7aed733594c5bb63514b9a
rikmarqk <rikmarqk@gmail.com>
2020-11-18 15:20:41 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,16 @@
 # your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name, bank_account=25.0, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+        
+        if happiness 
+    end
+
+end
